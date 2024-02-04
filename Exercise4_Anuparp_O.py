087ec85d1309bc74a723fba9b2bf236d4e864a93
NGK77292 <158686337+NGK77292@users.noreply.github.com>
2024-02-04 14:22:09 +0700

    Added exercise4
    Added exercise4 Anuparp O

---
+++ Exercise4_Anuparp_O.py
@@ -0,0 +1,9 @@
+eng = "90"
+buz = "88"
+com = "91"
+prog = "95"
+print("--- Your Score ---")
+print("Foundation English : ", eng)
+print("General Business : ", buz)
+print("Introduce to Computer System : ", com)
+print("Computer Programming : ", prog)
